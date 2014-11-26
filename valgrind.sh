1d2c1c2b04729e305db858a3677b4bf9aff4e63d
Christian Lundgren <christian.lundgren@gameanalytics.com>
2014-11-26 14:45:14 +0100

    Update valgrind script

--- valgrind.sh
+++ valgrind.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -e
+# See this doc on how to build the valgrind flavor beam:
+# http://www.erlang.org/doc/installation_guide/INSTALL.html
 export CFLAGS="$CFLAGS -g"
 ./rebar clean compile
-export VALGRIND_MISC_FLAGS="--suppressions=$ERL_TOP/erts/emulator/valgrind/suppress.standard --show-possibly-lost=no --dsymutil=yes --leak-check=full"
-$ERL_TOP/bin/cerl -valgrind
+rm -rf priv/erlbz2.so.dSYM
+export VALGRIND_MISC_FLAGS="--suppressions=$ERL_TOP/erts/emulator/valgrind/suppress.standard --show-possibly-lost=yes --dsymutil=yes --leak-check=full --show-leak-kinds=all --trace-children=yes -v"
+$ERL_TOP/bin/cerl -valgrind -pa ../erlbz2/ebin -smp enable
